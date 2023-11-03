3befbdf4a760ab325e2ea6398a1421bb715f1d78
abhikumar11 <abhishekkumar12verma@gmail.com>
2023-11-03 11:37:49 +0530

    initial commit

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import ToList from "./ToList";
 const Home = ({ user }) => {
      const [list, setList] = useState([]);
      useEffect(() => {
@@ -8,8 +9,7 @@ const Home = ({ user }) => {
 
      return (
      <div>
-      {user ? (
-      <p>{list}</p>
+      {user ? (<ToList list={list}/>
      ) : (
      <div>
       <h1>please login to view your todo list</h1>
--- src/components/ToList.js
+++ src/components/ToList.js
@@ -3,7 +3,12 @@ import React from 'react'
 const ToList = ({list}) => {
   return (
     <div>
-      
+      {list.map((item)=>(
+        <div>
+          <p>{item.task}</p>
+          <p>{item.time}</p>
+          </div>
+      ))}
     </div>
   )
 }
